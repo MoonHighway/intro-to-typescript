b374d92b32210f24fc7b061073cf1911215a1ddd
Simon Plenderleith <simon@simonplend.co.uk>
2024-06-09 18:37:47 -0700

    Add samples for structural typing and built-in predicates

---
+++ 02-types-in-action/samples/03-structural-typing/index.0.ts
@@ -0,0 +1,41 @@
+// -- Structural typing: Object literal without type annotation --
+
+{
+  type Point = {
+    x: number;
+    y: number;
+  };
+
+  function outputPoint(point: Point) {
+    console.log(`x: ${point.x}, y: ${point.y}`);
+  }
+
+  // Literal object that matches the structure of Point, but without a type annotation.
+  const point = { x: 10, y: 20 };
+
+  outputPoint(point);
+}
+
+// -- Structural typing: Objects with the same structure --
+
+{
+  type Car = {
+    wheels: number;
+    brand: string;
+  };
+
+  type Motorbike = {
+    wheels: number;
+    brand: string;
+  };
+
+  function outputCarDetails(car: Car) {
+    console.log(`Car Brand: ${car.brand}, Wheels: ${car.wheels}`);
+  }
+
+  const myBike: Motorbike = { wheels: 2, brand: "Yamaha" };
+
+  // This is allowed due to structural typing,
+  // even though we didn't intend for Motorbike to be assignable to Car.
+  outputCarDetails(myBike);
+}
--- 02-types-in-action/samples/04-type-narrowing/index.0.ts
+++ 02-types-in-action/samples/04-type-narrowing/index.0.ts
@@ -84,3 +84,116 @@
   console.log(formatCityName('Tokyo'));
   console.log(formatCityName('New York'));
 }
+
+// -- Types with built-in predicates --
+
+{
+  // -- string --
+
+  function capitalizeString(value?: string) {
+    if (typeof value === 'string') {
+      return value.toUpperCase();
+    }
+  }
+
+  capitalizeString();
+  capitalizeString('string');
+  capitalizeString(42);
+
+  // -- number --
+
+  function formatNumber(value?: number) {
+    if (typeof value === 'number') {
+      return value.toLocaleString();
+    }
+  }
+
+  formatNumber();
+  formatNumber(42);
+  formatNumber('boom')
+
+  // -- bigint --
+
+  function bigIntToString(value?: bigint) {
+    if (typeof value === 'bigint') {
+      return value.toString();
+    }
+  }
+
+  bigIntToString();
+  bigIntToString(42n);
+  bigIntToString(42)
+
+  // -- boolean --
+
+  function booleanToString(value?: boolean) {
+    if (typeof value === 'boolean') {
+      return value.toString();
+    }
+  }
+
+  booleanToString();
+  booleanToString(true);
+  booleanToString('true');
+
+  // -- symbol --
+
+  function getSymbolDescription(value?: symbol) {
+    if (typeof value === 'symbol') {
+      return value.description;
+    }
+  }
+
+  getSymbolDescription();
+  getSymbolDescription(Symbol('name'));
+  getSymbolDescription('name');
+
+  // -- undefined --
+
+  function isValueUndefined(value: undefined | null) {
+    if (typeof value === 'undefined') {
+      return true;
+    }
+
+    return false;
+  }
+
+  isValueUndefined(undefined);
+  isValueUndefined(null);
+
+  // -- function --
+
+  function runCallback(callback?: (message: string) => void) {
+    if (typeof callback === 'function') {
+      callback('Callback has been run');
+    }
+  }
+
+  runCallback();
+  runCallback((message) => console.log(message));
+  runCallback('callback');
+
+  // -- array --
+
+  function arrayToString(value?: Array<number>) {
+    if (Array.isArray(value)) {
+      return value.join(', ');
+    }
+  }
+
+  arrayToString();
+  arrayToString([1, 2, 3]);
+  arrayToString('1, 2, 3');
+
+  // -- object --
+
+  function getObjectKeys(obj?: object) {
+    if (typeof obj === 'object') {
+      return Object.keys(obj);
+    }
+  }
+
+  getObjectKeys();
+  getObjectKeys({ key1: 'value 1', key2: 'value 2' });
+  getObjectKeys('key1: value 1, key2: value 2');
+}
