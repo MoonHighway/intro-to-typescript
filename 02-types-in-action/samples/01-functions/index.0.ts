c27b3c0afecbd00d1c3c8f5279a87b80bea13cf7
Simon Plenderleith <simon@simonplend.co.uk>
2024-06-09 17:09:33 -0700

    Add samples for function overloads

--- 02-types-in-action/samples/01-functions/index.0.ts
+++ 02-types-in-action/samples/01-functions/index.0.ts
@@ -69,3 +69,48 @@
   const id = fetchId('https://jsonplaceholder.typicode.com/todos/1');
   console.log(id);
 }
+
+// -- Function overloads --
+
+{
+  // -- Example 1 --
+  // Function which accepts one or three arguments.
+
+  // Function overload signatures
+  function add(x: number): number;
+  function add(x: number, y: number, z: number): number;
+
+  // Implementation signature
+  function add(x: number, y?: number, z?: number): number {
+      if (y !== undefined && z !== undefined) {
+          return x + y + z;
+      } else {
+          return x + x;
+      }
+  }
+
+  console.log(add(5));
+
+  console.log(add(5, 7, 2));
+
+  // Error: No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments. ts(2575)
+  console.log(add(5, 7));
+
+  // -- Example 2 --
+  // Same number of arguments, but different types, use a union type instead of function overloads.
+
+  function multiply(value: number | { x: number, y: number }): number {
+      if (typeof value === 'number') {
+          return value * value;
+      } else {
+          return value.x * value.y;
+      }
+  }
+
+  console.log(multiply(5));
+
+  console.log(multiply({ x: 5, y: 7 }));
+
+  // Error: Argument of type '{ x: number; }' is not assignable to parameter of type 'number | { x: number; y: number; }'. ts(2345)
+  console.log(multiply({ x: 26 }));
+}
