9912f0ae99bc5a57662d33919a4c44dbb6afe8f3
Simon Plenderleith <simon@simonplend.co.uk>
2024-06-15 13:31:10 -0700

    Add samples for extending an interface and object index signatures

--- 03-flexible-types/samples/01-extending-an-interface/index.0.ts
+++ 03-flexible-types/samples/01-extending-an-interface/index.0.ts
@@ -1,1 +1,62 @@
 // -- Extending an interface --
+
+// `extend` copies members from an existing interface and allows us to add new ones.
+
+{
+  interface Country {
+    readonly name: string;
+    readonly code: string;
+    population?: number;
+  }
+
+  interface CountryWithCurrency extends Country {
+    currency: {
+      name: string;
+    };
+  }
+
+  const country1: Country = {
+    name: "Narnia",
+    code: "NN",
+    population: 256,
+  };
+
+  const country2: CountryWithCurrency = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5_135_300,
+    currency: {
+      name: "New Zealand dollar",
+    },
+  };
+}
+
+// -- Extending multiple interfaces --
+
+{
+  interface Country {
+    readonly name: string;
+    readonly code: string;
+    population?: number;
+  }
+
+  interface Currency {
+    currency: {
+      name: string;
+    };
+  }
+
+  interface CountryWithCurrencyLanguages extends Country, Currency {
+    languages: string;
+  }
+
+  const country3: CountryWithCurrencyLanguages = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5_135_300,
+    currency: {
+      name: "New Zealand dollar",
+    },
+    languages: "English, Maori",
+  };
+}
--- 03-flexible-types/samples/03-object-index-signatures/index.0.ts
+++ 03-flexible-types/samples/03-object-index-signatures/index.0.ts
@@ -1,1 +1,48 @@
 // -- Object index signatures --
+
+{
+  type CountryStats = {
+    name: string;
+    /**
+     * This index signature doesn't allow for defined members to have a
+     * type other than `number`.
+     */
+    // [key: string]: number;
+    /**
+     * This index signature allows for `name` to be a `string` and any
+     * other properties to have a tyoe of `number` or `string`.
+     */
+    // [key: string]: number | string;
+  }
+
+  const stats: CountryStats = {
+    name: "New Zealand"
+  };
+
+  // These properties are not allowed because they don't exist in the `CountryStats` type.
+  // Add an index signature above to allow them.
+  stats.population = 5_135_300
+  stats.area = 268_021
+}
+
+// -- Interface with an object index signature --
+
+{
+  interface CloudProvider {
+    name: string;
+    [key: string]: string | number | boolean;
+  }
+
+  const provider1: CloudProvider = {
+    name: "AWS",
+    region: "us-east-1",
+    services: 200,
+  };
+
+  const provider2: CloudProvider = {
+    name: "GCP",
+    region: "us-central1",
+    services: 150,
+    freeTier: true,
+  };
+}
