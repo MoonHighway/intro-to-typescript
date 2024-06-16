ffa0a2be529a56ab2c64137521c09df42c12fea8
Simon Plenderleith <simon@simonplend.co.uk>
2024-06-16 12:40:42 -0700

    fixup! WIP Blog API exercise

--- part-02/04-generics/exercise/src/app.ts
+++ part-02/04-generics/exercise/src/app.ts
@@ -1,3 +1,27 @@
+function renderPost(title: string, body: string) {
+  const $postTitle = document.getElementById("post-title");
+  const $postBody = document.getElementById("post-body");
+
+  if (!$postTitle || !$postBody) {
+    throw new Error("Post elements not found");
+  }
+
+  $postTitle.textContent = title;
+  $postBody.textContent = body;
+}
+
+function renderComment(name: string, body: string) {
+  const commentsList = document.getElementById("comments-list");
+
+  if (!commentsList) {
+    throw new Error("Comments list not found");
+  }
+
+  const commentElement = document.createElement("li");
+  commentElement.textContent = `${name} commented: "${body}"`;
+  commentsList.appendChild(commentElement);
+}
+
 {
   // No generic constraints
   // type Post = {
@@ -32,32 +56,6 @@
 }
 
 {
-  function renderPost(title: string, body: string) {
-    const $postTitle = document.getElementById("post-title");
-    const $postBody = document.getElementById("post-body");
-
-    if (!$postTitle || !$postBody) {
-      throw new Error("Post elements not found");
-    }
-
-    $postTitle.textContent = title;
-    $postBody.textContent = body;
-  }
-
-  function renderComment(name: string, body: string) {
-    const commentsList = document.getElementById("comments-list");
-
-    if (!commentsList) {
-      throw new Error("Comments list not found");
-    }
-
-    const commentElement = document.createElement("li");
-    commentElement.textContent = `${name} commented: "${body}"`;
-    commentsList.appendChild(commentElement);
-  }
-
-  // ----
-
   // Generic constraints
 
   interface ApiEntity {
